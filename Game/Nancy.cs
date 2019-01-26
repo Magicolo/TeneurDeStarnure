b00c5c30b7f6c28881a7588fd0264cf4c9863141
Richard <rail.rich@gmail.com>
2019-01-26 10:24:54 -0500

    Base server

---
+++ Game/Nancy.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+	public class RequestObject
+	{
+		public int Id { get; set; }
+	}
+
+	public class Nancy : NancyModule
+	{
+		public Nancy()
+		{
+
+			Get["/"] = _ => "Hello Kwame!";
+			Get["/Kwame&id={id}"] = value =>
+			{
+				var request = this.Bind<RequestObject>();
+				return "bong " + (request.Id + 1);
+			};
+			
+		}
+	}
+}
---
+++ Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HostConfiguration hostConfigs = new HostConfiguration();
+			//hostConfigs.UrlReservations.CreateAutomatically = true;
+			hostConfigs.RewriteLocalhost = true;
+			using (var host = new NancyHost(new Uri("http://localhost:6112"), new DefaultNancyBootstrapper(), hostConfigs))
+			{
+				host.Start();
+				Console.WriteLine("Running on http://192.168.1.203:8080");
+				Console.ReadLine();
+			}
+		}
+	}
+}
