b8e8792d8851ccc184b42a7131411de86bcdc0d0
Richard <rail.rich@gmail.com>
2019-01-26 12:52:08 -0500

    WOW

--- Game/Nancy.cs
+++ Game/Nancy.cs
@@ -16,14 +16,34 @@ namespace Game
 	{
 		public Nancy()
 		{
+			Get["/"] = parameters =>
+			{
+				return Response.AsFile("www/index.html", "text/html");
+			};
 
-			Get["/"] = _ => "Hello Kwame!";
-			Get["/Kwame&id={id}"] = value =>
+			Get["/Kwame/{id}"] = value =>
 			{
 				var request = this.Bind<RequestObject>();
-				return "bong " + (request.Id + 1);
+				return JsonResponse("{ type: \"text\", text: \"just kidding"+ (request.Id + 1)+"\" }");
+				//return "bong " + (request.Id + 1);
 			};
 			
+		}
+
+		public Response JsonResponse(string jsonString) {
+			byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+			return new Response()
+			{
+				StatusCode = HttpStatusCode.OK,
+				ContentType = "application/json",
+				//ReasonPhrase = "Because why not!",
+				Headers = new Dictionary<string, string>()
+				{
+					{ "Content-Type", "application/json" }
+					//{ "X-Custom-Header", "Sup?" }
+				},
+				Contents = c => c.Write(jsonBytes, 0, jsonBytes.Length)
+			};
 		}
 	}
 }
