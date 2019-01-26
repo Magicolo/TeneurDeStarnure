ff4823062fb3326f3deb1f37374b61d4bb55f08c
Magicolo <straty91@hotmail.com>
2019-01-26 14:03:46 -0500

    uiykrt

--- Game/Json.cs
+++ Game/Json.cs
@@ -1,16 +1,19 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Game
 {
 	public static class Json
 	{
-		public static string Serialize(this Event @event) => JsonConvert.SerializeObject(new
+		public static JObject ToJson(this Event @event) => JObject.FromObject(new
 		{
 			@event.Identifier,
 			Script = @event.Script.Build(@event.Script),
 			@event.Choices
 		});
 
+		public static JObject ToJson(this Result @object) => JObject.FromObject(@object);
+		public static JToken ToJson(this object @object) => JToken.FromObject(@object);
 		public static string Serialize(this object @object) => JsonConvert.SerializeObject(@object);
 	}
 }
--- Game/Result.cs
+++ Game/Result.cs
@@ -1,8 +1,10 @@
-﻿namespace Game
+﻿using Newtonsoft.Json.Linq;
+
+namespace Game
 {
 	public readonly struct Result
 	{
-		public readonly string Content;
+		public readonly JToken Content;
 		public readonly bool Success;
 
 		public Result(string content, bool success)
@@ -14,7 +16,7 @@
 
 	public static class ResultExtensions
 	{
-		public static Result ToResult<T>(this T value, bool success) => new Result(value.Serialize(), success);
+		public static Result ToResult<T>(this T value, bool success) => new Result(value.ToJson(), success);
 		public static Result ToSuccess<T>(this T value) => value.ToResult(true);
 		public static Result ToFailure<T>(this T value) => value.ToResult(false);
 	}
