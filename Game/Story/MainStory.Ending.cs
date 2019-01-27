e767540b349c69de4fde24920e01ca67995b28a7
Magicolo <straty91@hotmail.com>
2019-01-27 14:27:27 -0500

    asdoiu

---
+++ Game/Story/MainStory.Ending.cs
@@ -0,0 +1,12 @@
+﻿namespace Game
+{
+	public partial class State
+	{
+	}
+
+	public static partial class MainStory
+	{
+		public static readonly Event ColdEnding;
+		public static readonly Event HotEnding;
+	}
+}
--- Game/Story/MainStory.Kitchen.cs
+++ Game/Story/MainStory.Kitchen.cs
@@ -4,6 +4,8 @@ namespace Game
 {
 	public partial class State
 	{
+		public int MomKitchen;
+		public int DadKitchen;
 	}
 
 	public static partial class MainStory
@@ -16,11 +18,71 @@ namespace Game
 				Condition.IsCharacter(Characters.Lau),
 				Effect.GoTo(nameof(LivingRoom))
 			),
+			new Choice("Go outside.",
+				Sequence(
+					Line("The wand draws you outside... Finally the whispers overcome you."),
+					Line("...").Typewrite(5),
+					Line("All is cold...").Typewrite(10),
+					Line("All is silent...").Typewrite(7),
+					Line("All is lost.").Typewrite(4)
+				),
+				(state, player) => player.Character.Identifier == Characters.Lau && state.MomKitchen >= 2 && state.DadKitchen >= 2,
+				Effect.GoTo(nameof(ColdEnding))
+			),
 
 			new Choice("Turn on the stove.",
-				Line("It's a gas stove, so it still works."),
-				Condition.IsCharacter(Characters.Mom),
-				Effect.GoTo(nameof(LivingRoom))
+				Sequence(
+					Line("It's a gas stove, so it hopefully still works."),
+					Line("...").Typewrite(5),
+					Line("The whispers are angry.")
+				),
+				(state, player) => player.Character.Identifier == Characters.Mom && state.MomKitchen == 0,
+				state => state.MomKitchen++
+			),
+			new Choice("Put towel on stove.",
+				Sequence(
+					Line("The whispers."),
+					Line("...").Typewrite(5),
+					Line("THE WHISPERS!").Bold()
+				),
+				(state, player) => player.Character.Identifier == Characters.Mom && state.MomKitchen == 1 && state.DadKitchen == 1,
+				state => state.MomKitchen++
+			),
+			new Choice("Go outside.",
+				Sequence(
+					Line("The wand draws you outside... Finally the whispers overcome you."),
+					Line("...").Typewrite(5),
+					Line("All is cold...").Typewrite(10),
+					Line("All is silent...").Typewrite(7),
+					Line("All is lost.").Typewrite(4)
+				),
+				(state, player) => player.Character.Identifier == Characters.Mom && state.MomKitchen >= 2 && state.DadKitchen >= 2,
+				Effect.GoTo(nameof(ColdEnding))
+			),
+
+			new Choice("Grab towel.",
+				Line("It's soft."),
+				(state, player) => player.Character.Identifier == Characters.Dad && state.DadKitchen == 0,
+				state => state.DadKitchen++
+			),
+			new Choice("PUT TOWEL ON THE STOVE.",
+				Sequence(
+					Line("The towel seems to leap out of hand onto the stove."),
+					Line(".....").Typewrite(5),
+					Line("The towel starts burning."),
+					Line(".....").Typewrite(5),
+					Line("It spreads. The whispers are manic.")
+				),
+				(state, player) => player.Character.Identifier == Characters.Mom && state.MomKitchen == 2 && state.DadKitchen == 1,
+				state => state.DadKitchen++
+			),
+			new Choice("Stay.",
+				Sequence(
+					Line("The whispers scream to go outside, but you call on Dad's stubborn attitude and watch as the flames engulf the room."),
+					Line("Finally it can end...").Typewrite(8)
+				),
+				(state, player) => player.Character.Identifier == Characters.Dad && state.MomKitchen == 2 && state.DadKitchen == 2,
+				Effect.GoTo(nameof(HotEnding))
 			)
 		);
 	}
